b67f7567f70cc0086bb85687875c0f7526821f7d
Kevin Cunanan <kevin.a.cunanan@gmail.com>
2019-01-20 13:00:57 -0800

    added basic login page

--- ui/src/components/Login/Login.tsx
+++ ui/src/components/Login/Login.tsx
@@ -1,5 +1,17 @@
 import * as React from "react";
 
+import {Button, Col, Input, Row} from "antd";
+
+import "./Login.less";
+
 export default () => (
-    <h1>Login</h1>
+    <React.Fragment>
+        <Row className="login-row">
+            <Col span={12} offset={6}>
+                <Input size="large" placeholder="Login" />
+                <Input size="large" type="password" placeholder="Password" />
+                <Button className="login-button" type="primary">Login</Button>
+            </Col>
+        </Row>
+    </React.Fragment>
 );

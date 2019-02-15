d9478ecfdec73b2a0ec1d83566403dca87d4ea42
Kevin Cunanan <kevin.a.cunanan@gmail.com>
2019-02-15 14:40:33 -0800

    created column_header app

---
+++ api/src/apps/column_headers/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ColumnHeadersConfig(AppConfig):
+    name = 'column_headers'
---
+++ api/src/apps/column_headers/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from apps.column_headers.choice import Choice
+from django.core.validators import validate_slug
+
+class ColumnHeaderType(Choice):
+    TEXT = "text"
+    NUMBER = "number"
+    LONG_TEXT = "long_text"
+    HTML = "html"
+    URL = "url"
+    IMAGE = "image"
+
+    CHOICES = (
+        (TEXT, "Text"),
+        (NUMBER, "Number"),
+        (LONG_TEXT, "Long Text"),
+        (HTML, "html"),
+        (URL, "URL"),
+        (IMAGE, "Image"),
+    )
+
+class ColumnHeader(models.Model):
+    name = models.CharField(max_length=15, blank=False)
+    type = models.CharField(max_length=50, default=ColumnHeaderType.TEXT, choices=ColumnHeaderType.CHOICES)
+    order = models.PositiveSmallIntegerField()
+    slug = models.SlugField(max_length=15)

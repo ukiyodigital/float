3242a182bc69f7f5b7afe9191337a52ea58cc052
Kevin Cunanan <kevin.a.cunanan@gmail.com>
2019-02-15 13:57:15 -0800

    removed devServer host configuration changes

--- ui/tools/devServer.js
+++ ui/tools/devServer.js
@@ -3,9 +3,6 @@ const webpack =  require("webpack");
 const WebpackDevServer =  require("webpack-dev-server");
 const config =  require("../webpack.config");
 
-const host = "0.0.0.0";
-
-config.entry.unshift(`webpack-dev-server/client?http://${host}:443/`, "webpack/hot/dev-server");
 
 const compiler = webpack(config);
 const options = {
@@ -17,4 +14,4 @@ const options = {
 };
 const server = new WebpackDevServer(compiler, options);
 
-server.listen(3000, host);
+server.listen(3000);

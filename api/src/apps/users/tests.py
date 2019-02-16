f4d98c30b1739009b8b041a8b7a1fa9af6d827d6
Kevin Cunanan <kevin.a.cunanan@gmail.com>
2019-02-16 13:19:01 -0800

    added test for login

--- api/src/apps/users/tests.py
+++ api/src/apps/users/tests.py
@@ -9,13 +9,22 @@ from rest_framework.test import APITestCase
 
 from apps.users.models import User
 from rest_framework.authtoken.models import Token
-from apps.users.serializers import UserSerializer
+from apps.users.serializers import UserSerializer, CreateUserSerializer
 
 
 class UserTestCase(APITestCase):
     """
     python ./manage.py test apps/users
     """
+    def setUp(self):
+        data = {
+            "username": "tester",
+            "password": "password",
+            "email": "tester@email.com"
+        }
+        serializer = CreateUserSerializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
 
     def test_create_user(self):
         data = {
@@ -29,9 +38,13 @@ class UserTestCase(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, {"token": token.key})
 
-    # def test_user_login(self):
-    #     url = reverse("auth")
-    #     response = self.client.get(url, content_type="application/json")
-    #     user = User.objects.get(username=self.user.username)
-    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
-    #     self.assertEqual(response.data, UserSerializer(user).data)
+    def test_user_login(self):
+        data = {
+            "username": "tester",
+            "password": "password"
+        }
+        response = self.client.post(reverse("login"), json.dumps(data), content_type="application/json")
+        user = User.objects.get(username=data["username"])
+        token = Token.objects.get(user=user)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, {"token": token.key})

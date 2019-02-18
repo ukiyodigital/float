80b82bf3815fb766a339106d921a68c208049f1b
Kevin Cunanan <kevin.a.cunanan@gmail.com>
2019-02-18 12:42:09 -0800

    added endpoint for site

--- api/src/apps/sites/views.py
+++ api/src/apps/sites/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from apps.sites.serializers import SiteCreateSerializer, SiteListSerializer, SiteDetailSerializer
+from apps.sites.models import Site
+from rest_framework.response import Response
 
-# Create your views here.
+
+class ListCreateSites(ListCreateAPIView):
+    """
+    Endpoint for creating new sites, and also listing out current ones accessible by the user
+    """
+    permission_classes = (IsAuthenticated,)
+    # TODO list out sites you only have access to
+    queryset = Site.objects.all()
+
+    def get_serializer_class(self):
+        if self.request.method == "POST":
+            return SiteCreateSerializer
+        return SiteListSerializer
+
+    def post(self, request, *args, **kwargs):
+        super().post(request, *args, **kwargs)
+        site = Site.objects.filter(slug=request.data["slug"]).first()
+        return Response(SiteDetailSerializer(site).data)
+
+
+
+# TODO Add Update / Retrieve singular site

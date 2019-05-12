4012c564914d6f41cdbe15f4e22e79818a8eb353
Kevin Cunanan <kevin.a.cunanan@gmail.com>
2019-05-12 00:06:53 -0700

    refactored base class so authentication happens in this class

--- api/src/apps/float/tests.py
+++ api/src/apps/float/tests.py
@@ -1,20 +1,29 @@
 import json
-from apps.users.models import User
+
+from rest_framework.test import APITestCase, APIClient
+
+from rest_framework.authtoken.models import Token
+
 from apps.sites.models import Site
 from apps.pages.models import Page
-from rest_framework.test import APITestCase
+from apps.users.models import User
+from apps.users.serializers import UserSerializer, CreateUserSerializer
 
 class AppTestCase(APITestCase):
+    def setUp(self):
+        self.setUpAuthUser()
+
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-
 
     @classmethod
     def setUpTestData(cls):
         with open("/app/apps/float/data/mockdata.json") as f:
             mock_data = json.loads(f.read())
 
+        cls.client = APIClient()
+        cls.mock_users_data = mock_data["users"]
         cls.users = cls._create_users(mock_data["users"])
         cls.sites = cls._create_sites(mock_data["sites"])
         cls.pages = cls._create_pages(mock_data["pages"])
@@ -22,7 +31,9 @@ class AppTestCase(APITestCase):
     @classmethod
     def _create_users(cls, mock_users):
         for user in mock_users:
-            user = User.objects.get_or_create(**user)
+            serializer = CreateUserSerializer(data=user)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
 
         return User.objects.all()
 
@@ -43,3 +54,7 @@ class AppTestCase(APITestCase):
             page.save()
 
         return Page.objects.all()
+
+    def setUpAuthUser(self):
+        token, _ = Token.objects.get_or_create(user=self.users[0])
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)

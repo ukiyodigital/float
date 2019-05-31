d3b3fe5556938e08c55b39580bda1214303e6604
Kevin Cunanan <kevin.a.cunanan@gmail.com>
2019-05-30 23:19:55 -0700

    updated and squashed migrations since model choice object conflicted with graphql structure

--- api/src/apps/column_headers/models.py
+++ api/src/apps/column_headers/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from apps.column_headers.choice import Choice
 from django.core.validators import validate_slug
 
-class ColumnHeaderType(Choice):
+class ColumnHeaderField(Choice):
     TEXT = "text"
     NUMBER = "number"
     LONG_TEXT = "long_text"
@@ -21,7 +21,7 @@ class ColumnHeaderType(Choice):
 
 class ColumnHeader(models.Model):
     name = models.CharField(max_length=15, blank=False)
-    type = models.CharField(max_length=50, default=ColumnHeaderType.TEXT, choices=ColumnHeaderType.CHOICES)
+    field = models.CharField(max_length=50, default=ColumnHeaderField.TEXT, choices=ColumnHeaderField.CHOICES)
     order = models.PositiveSmallIntegerField(default=0)
     slug = models.SlugField(max_length=15)
 

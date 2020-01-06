a74bd386a106b952915a051d9379e5e1713e7a6d
Kevin Cunanan <kevin.a.cunanan@gmail.com>
2020-01-05 18:20:43 -0800

    model creation for flocks

---
+++ api/src/apps/flocks/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.contrib.postgres.fields import ArrayField, JSONField
+from django.core.serializers.json import DjangoJSONEncoder
+
+from apps.sites.models import Site
+from apps.column_headers.models import ColumnHeader
+
+
+class Flock(models.Model):
+    name = models.CharField(max_length=15, blank=False)
+    slug = models.SlugField(max_length=15)
+
+    # where repeated data is stored
+    data = ArrayField(
+        JSONField(null=True, blank=True, encoder=DjangoJSONEncoder),
+        blank=True,
+    )
+
+    # Foreign Keys
+    site = models.ForeignKey(Site, on_delete=models.CASCADE, related_name='flocks')
+
+    class Meta:
+        unique_together = ('slug', 'site',)
+
+
+class FlockColumnHeader(ColumnHeader):
+    flock = models.ForeignKey(Flock, on_delete=models.CASCADE, related_name='columns')
+
+    class Meta:
+        unique_together = ('flock', 'slug',)

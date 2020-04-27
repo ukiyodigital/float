451304f0ae6c729054956bcb7b0315d586f7e936
Kevin Cunanan <kevin.a.cunanan@gmail.com>
2020-04-27 01:41:20 -0700

    added private routing

--- ui/src/components/RouteNode/RouteNode.jsx
+++ ui/src/components/RouteNode/RouteNode.jsx
@@ -1,21 +1,52 @@
-import * as React from 'react';
-import { Route } from 'react-router-dom';
-import routes from '../../routing/routes';
+import React from 'react';
 
-const RouteNode = () => (
-  <>
-    {routes.map(
-      (route) => (
-        <Route
-          key={route.name}
-          path={route.path}
-          exact={route.exact}
-          component={route.component}
-          name={route.name}
-        />
-      ),
-    )}
-  </>
-);
+import { useQuery } from '@apollo/react-hooks';
+
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+import routes from '_/routing/routes';
+
+import IS_LOGGED_IN from '_/apollo/queries';
+
+
+const RouteNode = () => {
+  const { data: { isLoggedIn } } = useQuery(IS_LOGGED_IN);
+
+  return (
+    <Switch>
+      {routes.map(
+        (route) => (
+          route.loginRequired ? (
+            <Route
+              // eslint-disable-next-line react/jsx-props-no-spreading
+              {...route}
+              component={null}
+              key={route.name}
+              render={({ location }) => (
+                isLoggedIn ? (
+                  route.component
+                ) : (
+                  <Redirect
+                    to={{
+                      pathname: '/login',
+                      state: { from: location },
+                    }}
+                  />
+                )
+              )}
+            />
+          ) : (
+            <Route
+              key={route.name}
+              path={route.path}
+              exact={route.exact}
+              component={route.component}
+              name={route.name}
+            />
+          )),
+      )}
+    </Switch>
+  );
+};
 
 export default RouteNode;

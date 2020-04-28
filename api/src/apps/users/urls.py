46542058922b0d0bf3a61d76330d515f8881dde8
Kevin Cunanan <kevin.a.cunanan@gmail.com>
2020-04-27 23:43:44 -0700

    removed all serializers and tests, django rest is not a dependency

--- api/src/apps/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf import settings
-from django.urls import path
-from .views import UserSignup, UserLogin
-
-urlpatterns = [
-    path('login/', UserLogin.as_view(), name='login'),
-    path('signup/', UserSignup.as_view(), name='signup')
-]

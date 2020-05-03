06642e07d6f5cdc665cd96c464e0b56af52394c2
Kevin Cunanan <kevin.a.cunanan@gmail.com>
2020-05-03 13:36:55 -0700

    WIP added siteslug

--- ui/src/routing/routes.js
+++ ui/src/routing/routes.js
@@ -9,7 +9,10 @@ export default [
     name: 'sites', path: '/', exact: true, component: Sites, loginRequired: true,
   },
   {
-    name: 'create-site', path: '/sites/create', exact: true, component: CreateSite, loginRequired: true,
+    name: 'create-site', path: '/create', exact: true, component: CreateSite, loginRequired: true,
+  },
+  {
+    name: 'site-detail', path: '/:siteSlug', exact: true, component: SiteDetail, loginRequired: true,
   },
   {
     name: 'login', path: '/login', exact: true, component: Login,

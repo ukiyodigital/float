5c7a7e9dcd2c12aa899e8031cc718aabd04724cd
Kevin Cunanan <kevin.a.cunanan@gmail.com>
2020-05-08 18:07:40 -0700

    added detail view and added both to routes file

---
+++ ui/src/components/SiteDetail/SiteDetail.jsx
@@ -0,0 +1,99 @@
+import React from 'react';
+
+import { makeStyles } from '@material-ui/core/styles';
+import { useQuery } from '@apollo/react-hooks';
+import { useParams } from 'react-router-dom';
+
+import {
+  Container, Divider, Grid, Typography, Paper,
+} from '@material-ui/core';
+
+import { GetSite } from '_/apollo/queries';
+
+import Loading from '_/components/Common/Loading/Loading';
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  title: {
+    marginTop: -20,
+  },
+  paper: {
+    padding: theme.spacing(2),
+  },
+  numberCard: {
+    width: '100%',
+    height: 200,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    textAlign: 'center',
+  },
+  divider: {
+    margin: '25px auto',
+  },
+}));
+
+const SiteDetail = () => {
+  const classes = useStyles();
+  const { siteSlug: slug } = useParams();
+  const {
+    loading,
+    data: {
+      site,
+    } = {},
+  } = useQuery(GetSite, {
+    variables: { slug },
+  });
+
+  return loading ? <Loading loading /> : (
+    <Container className={classes.root}>
+      <Grid spacing={3}>
+        <Typography variant="h2">
+          {site.name}
+        </Typography>
+        <Grid item xs={12} className={classes.root}>
+          <Grid container spacing={3} justify="space-between">
+            <Grid item xs={3}>
+              <Paper className={classes.numberCard}>
+                <Typography variant="h2" className={classes.title}>
+                  {site.pages.length}
+                </Typography>
+                <Typography variant="h5">
+                  Pages
+                </Typography>
+              </Paper>
+            </Grid>
+            <Grid item xs={8}>
+              <Paper className={classes.paper}>Test</Paper>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Divider className={classes.divider} />
+      <Grid spacing={3}>
+        <Grid item xs={12}>
+          <Grid container spacing={3} justify="space-between">
+            <Grid item xs={3}>
+              <Paper className={classes.numberCard}>
+                <Typography variant="h2" className={classes.title}>
+                  {site.flocks.length}
+                </Typography>
+                <Typography variant="h5">
+                  Collections
+                </Typography>
+              </Paper>
+            </Grid>
+            <Grid item xs={8}>
+              <Paper className={classes.paper}>Test</Paper>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
+
+export default SiteDetail;
--- ui/src/routing/routes.js
+++ ui/src/routing/routes.js
@@ -1,18 +1,19 @@
 import Login from '_/components/Login/Login';
 import Signup from '_/components/Signup/Signup';
 
-import Sites from '_/components/Sites/Sites';
+import SiteList from '_/components/SiteList/SiteList';
+import SiteDetail from '_/components/SiteDetail/SiteDetail';
 import CreateSite from '_/components/CreateSite/CreateSite';
 
 export default [
   {
-    name: 'sites', path: '/', exact: true, component: Sites, loginRequired: true,
+    name: 'sites', path: '/', exact: true, component: SiteList, loginRequired: true,
   },
   {
     name: 'create-site', path: '/create', exact: true, component: CreateSite, loginRequired: true,
   },
   {
-    name: 'site-detail', path: '/:siteSlug', exact: true, component: , loginRequired: true,
+    name: 'site-detail', path: '/:siteSlug', exact: true, component: SiteDetail, loginRequired: true,
   },
   {
     name: 'login', path: '/login', exact: true, component: Login,

013a614efa5295eb2a709cc39de6d09775aafc8e
kcunanan <kevin.a.cunanan@gmail.com>
2020-07-03 20:57:33 -0700

    added a basic permanaent drawer

---
+++ ui/src/components/Layout/Navigation/Drawer/Drawer.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+
+import { makeStyles } from '@material-ui/core/styles';
+
+import {
+  Divider, Drawer, List, ListItem, ListItemText,
+} from '@material-ui/core';
+
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  toolbar: theme.mixins.toolbar,
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+}));
+
+const AppDrawer = () => {
+  const classes = useStyles();
+  return (
+    <Drawer
+      className={classes.drawer}
+      classes={{
+        paper: classes.drawerPaper,
+      }}
+      variant="permanent"
+      open
+    >
+      <div className={classes.toolbar} />
+      <Divider />
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </Drawer>
+  );
+};
+
+export default AppDrawer;

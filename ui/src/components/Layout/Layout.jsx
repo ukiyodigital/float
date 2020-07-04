d83e9a65751836d18aaed4899de9e74d2de4c1ae
kcunanan <kevin.a.cunanan@gmail.com>
2020-07-03 20:59:13 -0700

    used react-router routing logic to display the right layout to add sidebar where appropriate

--- ui/src/components/Layout/Layout.jsx
+++ ui/src/components/Layout/Layout.jsx
@@ -2,31 +2,67 @@ import React from 'react';
 
 import { makeStyles } from '@material-ui/core/styles';
 import { Box, Container } from '@material-ui/core';
+import { Switch, Route } from 'react-router-dom';
 
-import Navigation from '_/components/Layout/Navigation/Navigation';
+import AppDrawer from '_/components/Layout/Navigation/Drawer/Drawer';
+import TopNav from '_/components/Layout/Navigation/Navigation';
 import RouteNode from '_/components/RouteNode/RouteNode';
 
+import { routes } from '_/routing/routes';
 
-const useStyles = makeStyles({
+
+const useStyles = makeStyles((theme) => ({
   root: {
     height: '100%',
   },
+  flex: {
+    display: 'flex',
+  },
   container: {
     marginTop: 25,
-    height: 'calc(100% - (25px + 64px))',
     position: 'relative',
   },
-});
+  sidebarContainer: {
+    marginTop: 25,
+    position: 'relative',
+    flexGrow: 1,
+  },
+  toolbar: theme.mixins.toolbar,
+}));
+
+const sidebarPaths = routes.filter((route) => route.sidebar).map((route) => route.path);
+const nonSidebarPaths = routes.filter((route) => !route.sidebar).map((route) => route.path);
 
 export default () => {
   const classes = useStyles();
 
   return (
     <Box component="div" className={classes.root}>
-      <Navigation />
-      <Container className={classes.container}>
-        <RouteNode />
-      </Container>
+      <div className={classes.flex}>
+        <Switch>
+          <Route path={sidebarPaths}>
+            <>
+              <TopNav hasSidebar />
+              <AppDrawer />
+              <Container className={classes.sidebarContainer}>
+                <div className={classes.toolbar} />
+                <RouteNode />
+              </Container>
+            </>
+          </Route>
+          <Route
+            path={nonSidebarPaths}
+          >
+            <>
+              <TopNav />
+              <Container className={classes.container}>
+                <div className={classes.toolbar} />
+                <RouteNode />
+              </Container>
+            </>
+          </Route>
+        </Switch>
+      </div>
     </Box>
   );
 };

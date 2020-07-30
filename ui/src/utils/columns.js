512efc4c08a45902adef3bac26fb60c2f7ac8dfc
kcunanan <kevin.a.cunanan@gmail.com>
2020-07-29 21:27:00 -0700

    extracted columns fns into reusable fn

---
+++ ui/src/utils/columns.js
@@ -0,0 +1,38 @@
+import { v4 as uuidv4 } from 'uuid';
+
+export const addColumn = (columns, setColumns) => {
+  setColumns([
+    ...columns,
+    {
+      id: uuidv4(),
+      name: '',
+      slug: '',
+      field: 'TEXT',
+      value: '',
+      unsaved: true,
+    },
+  ]);
+};
+
+export const updateColumn = (column, columns, setColumns) => {
+  const columnIdx = columns.findIndex((c) => c.id === column.id);
+  setColumns([
+    ...columns.slice(0, columnIdx),
+    column,
+    ...columns.slice(columnIdx + 1),
+  ]);
+};
+
+export const deleteColumn = (column, columns, setColumns) => {
+  const columnIdx = columns.findIndex((c) => c.id === column.id);
+  setColumns([
+    ...columns.slice(0, columnIdx),
+    ...columns.slice(columnIdx + 1),
+  ]);
+};
+
+export default {
+  addColumn,
+  updateColumn,
+  deleteColumn,
+};

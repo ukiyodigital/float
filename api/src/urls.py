d884c57d88a16433235749103aa333ce36e55e55
kcunanan <kevin.a.cunanan@gmail.com>
2020-08-05 01:40:11 -0700

    removed extra api/ path

--- api/src/urls.py
+++ api/src/urls.py
@@ -4,8 +4,8 @@ from graphene_django.views import GraphQLView
 
 
 urlpatterns = [
-    path('api/graphql/', GraphQLView.as_view(graphiql=True))
+    path('graphql/', GraphQLView.as_view(graphiql=True))
 ]
 
 if "silk" in settings.INSTALLED_APPS:
-    urlpatterns += [path('api/silk/', include('silk.urls'), name="silk")]
+    urlpatterns += [path('silk/', include('silk.urls'), name="silk")]

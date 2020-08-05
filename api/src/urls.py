18b58d4d5a39edf223b23a99d97f338afa2f7778
kcunanan <kevin.a.cunanan@gmail.com>
2020-08-04 22:10:34 -0700

    changed to add proxy prefix

--- api/src/urls.py
+++ api/src/urls.py
@@ -4,8 +4,8 @@ from graphene_django.views import GraphQLView
 
 
 urlpatterns = [
-    path('graphql/', GraphQLView.as_view(graphiql=True))
+    path('api/graphql/', GraphQLView.as_view(graphiql=True))
 ]
 
 if "silk" in settings.INSTALLED_APPS:
-    urlpatterns += [path('silk/', include('silk.urls'), name="silk")]
+    urlpatterns += [path('api/silk/', include('silk.urls'), name="silk")]

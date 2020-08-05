8596069332a2433f00435eaf7c7a34f7f1f71071
kcunanan <kevin.a.cunanan@gmail.com>
2020-08-05 15:50:46 -0700

    added deploy script for api and nginx

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+ssh -o StrictHostKeyChecking=no ec2-user@$EC2_PUBLIC_IP_ADDRESS << 'ENDSSH'
+  cd /home/ec2-user/app
+  export $(cat .env | xargs)
+  docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+  docker pull floatcms:api
+  docker pull floatcms:nginx
+  docker-compose -f docker-compose.prod.yml up -d
+ENDSSH

bd9be4f074cb7838288ab3f96f097161933c125e
kcunanan <kevin.a.cunanan@gmail.com>
2020-08-05 16:47:35 -0700

    cat .env file then remove

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,8 @@
 
 ssh -o StrictHostKeyChecking=no ec2-user@$EC2_PUBLIC_IP_ADDRESS << 'ENDSSH'
   cd /home/ec2-user/app
-  export $(cat .env | xargs)
+  export $(cat .env)
+  rm .env
   docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
   docker pull floatcms:api
   docker pull floatcms:nginx

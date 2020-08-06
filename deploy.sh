93440983ed7706e9c775cf2c06aabc9ce709aa97
kcunanan <kevin.a.cunanan@gmail.com>
2020-08-05 18:56:40 -0700

    fixed repository values for docker

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ ssh -o StrictHostKeyChecking=no ec2-user@$EC2_PUBLIC_IP_ADDRESS << 'ENDSSH'
   export $(cat .env)
   rm .env
   docker login -u $DOCKER_USERNAME -p $DOCKER_TOKEN
-  docker pull floatcms:api
-  docker pull floatcms:nginx
+  docker pull floatcms/api
+  docker pull floatcms/nginx
   docker-compose -f docker-compose.prod.yml up -d
 ENDSSH

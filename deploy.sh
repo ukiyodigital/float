7049667ad8093e066372e13c803f3e950ed29efe
kcunanan <kevin.a.cunanan@gmail.com>
2020-08-05 18:46:42 -0700

    changed to docker-token :facepalm

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ ssh -tt -o StrictHostKeyChecking=no ec2-user@$EC2_PUBLIC_IP_ADDRESS << 'ENDSSH'
   cd /home/ec2-user/app
   export $(cat .env)
   rm .env
-  docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+  docker login -u $DOCKER_USERNAME -p $DOCKER_TOKEN
   docker pull floatcms:api
   docker pull floatcms:nginx
   docker-compose -f docker-compose.prod.yml up -d

3f25f12505b9f12f3c161b1db3ad51140739ec38
kcunanan <kevin.a.cunanan@gmail.com>
2020-08-13 02:34:25 -0700

    created column input component slightly different from regular input

---
+++ ui/src/components/Common/ColumnInput/ColumnInput.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { Controller } from 'react-hook-form';
+import { TextField } from '@material-ui/core';
+
+import AppPropTypes from '_/proptypes';
+
+const ColumnInput = ({
+  field, message, value, ...props
+}) => (
+  <Controller
+    as={TextField}
+    onChange={([{ target: { value: v } }]) => {
+      field.onChange({ value: v });
+      return v;
+    }}
+    name={field.name}
+    label={field.label}
+    type={field.type}
+    defaultValue={value?.value}
+    helperText={message}
+    {...props}
+  />
+);
+
+ColumnInput.propTypes = {
+  message: PropTypes.string,
+  value: PropTypes.string.isRequired,
+  field: AppPropTypes.input.isRequired,
+};
+
+ColumnInput.defaultProps = {
+  message: '',
+};
+
+export default ColumnInput;

06e4a275c84e3edea6793d10e421a23c76d04592
kcunanan <kevin.a.cunanan@gmail.com>
2020-08-12 22:06:33 -0700

    moved storage backends

---
+++ api/src/apps/float/storage_backends.py
@@ -0,0 +1,9 @@
+from storages.backends.s3boto3 import S3Boto3Storage
+from settings import settings
+
+
+class MediaStorage(S3Boto3Storage):
+    location = settings.AWS_MEDIA_LOCATION
+    default_acl = 'private'
+    file_overwrite = False
+    custom_domain = False

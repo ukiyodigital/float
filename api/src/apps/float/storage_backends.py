b81f596f547a5c33c729bb7d700956d069af57a9
kcunanan <kevin.a.cunanan@gmail.com>
2020-08-17 13:50:58 -0700

    added fallback for aws_media_location

--- api/src/apps/float/storage_backends.py
+++ api/src/apps/float/storage_backends.py
@@ -3,7 +3,7 @@ from settings import settings
 
 
 class MediaStorage(S3Boto3Storage):
-    location = settings.AWS_MEDIA_LOCATION
+    location = settings.AWS_MEDIA_LOCATION if settings.AWS_MEDIA_LOCATION else 'media'
     default_acl = 'private'
     file_overwrite = False
     custom_domain = False

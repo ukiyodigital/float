d8b1f29a40b4d66c6bc68e2686d894afc2b4032a
kcunanan <kevin.a.cunanan@gmail.com>
2020-08-17 19:26:45 -0700

    updated the script

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,8 @@ ssh -o StrictHostKeyChecking=no ec2-user@$EC2_PUBLIC_IP_ADDRESS << 'ENDSSH'
   cd /home/ec2-user/app
   export $(cat .env)
   rm .env
-  docker login -u $DOCKER_USERNAME -p $DOCKER_TOKEN
+  echo $DOCKER_TOKEN | docker login -u $DOCKER_USERNAME --password-stdin
+  docker system prune -f
   docker pull floatcms/api
   docker pull floatcms/nginx
   docker-compose -f docker-compose.prod.yml up -d

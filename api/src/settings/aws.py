9c3ec8eff7d2f99735d95650ccc9b62a0bf1b19e
kcunanan <kevin.a.cunanan@gmail.com>
2020-09-03 16:08:08 -0700

    fixed double quotes, used get on os.environ

--- api/src/settings/aws.py
+++ api/src/settings/aws.py
@@ -2,19 +2,19 @@ import os
 import json
 import boto3
 
-USE_SSM = os.environ.get("USE_SSM") == "True"
-USE_S3 = os.environ.get("USE_S3") == "True"
-USE_ACCESS_KEYS = os.environ.get("USE_ACCESS_KEYS") == "True"
+USE_SSM = os.environ.get('USE_SSM') == 'True'
+USE_S3 = os.environ.get('USE_S3') == 'True'
+USE_ACCESS_KEYS = os.environ.get('USE_ACCESS_KEYS') == 'True'
 
-IS_LOCALSTACK = os.environ["IS_LOCALSTACK"] == 'True'
+IS_LOCALSTACK = os.environ.get('IS_LOCALSTACK') == 'True'
 
 if USE_SSM:
     # SSM setup
     ssm = boto3.client(
         'ssm',
         region_name='us-west-2',
-        aws_access_key_id=os.environ["SSM_ACCESS_KEY_ID"],
-        aws_secret_access_key=os.environ["SSM_SECRET_ACCESS_KEY"],
+        aws_access_key_id=os.environ['SSM_ACCESS_KEY_ID'],
+        aws_secret_access_key=os.environ['SSM_SECRET_ACCESS_KEY'],
     ) if USE_ACCESS_KEYS else boto3.client('ssm', region_name='us-west-2')
 
     SECRET_PATH = '/Float/API/'
@@ -32,7 +32,7 @@ if USE_SSM:
 
     # Database
     DATABASES = {
-        "default": {
+        'default': {
             'ENGINE': 'django.db.backends.postgresql',
             'NAME': _get_ssm_key('DJANGO_DB_NAME'),
             'USER': _get_ssm_key('DJANGO_DB_USER'),
@@ -45,15 +45,15 @@ if USE_SSM:
 
 if USE_S3:
     if USE_ACCESS_KEYS:
-        AWS_ACCESS_KEY_ID = os.environ["S3_ACCESS_KEY_ID"]
-        AWS_SECRET_ACCESS_KEY = os.environ["S3_SECRET_ACCESS_KEY"]
+        AWS_ACCESS_KEY_ID = os.environ['S3_ACCESS_KEY_ID']
+        AWS_SECRET_ACCESS_KEY = os.environ['S3_SECRET_ACCESS_KEY']
 
     # S3 Config
-    AWS_STORAGE_BUCKET_NAME = os.environ["AWS_BUCKET_NAME"]
+    AWS_STORAGE_BUCKET_NAME = os.environ['AWS_BUCKET_NAME']
     AWS_MEDIA_LOCATION = 'media'
 
     if IS_LOCALSTACK:
-        AWS_S3_ENDPOINT_URL = os.environ.get("AWS_S3_ENDPOINT_URL")
+        AWS_S3_ENDPOINT_URL = os.environ.get('AWS_S3_ENDPOINT_URL')
         MEDIA_URL = f'{AWS_S3_ENDPOINT_URL}/{AWS_STORAGE_BUCKET_NAME}/{AWS_MEDIA_LOCATION}/'
     else:
         AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'

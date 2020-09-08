1c61687088e4bb80dd9dec15de1a75e48d1728bd
kcunanan <kevin.a.cunanan@gmail.com>
2020-09-08 11:34:33 -0700

    added subcolumn logic

--- ui/src/utils/columns.js
+++ ui/src/utils/columns.js
@@ -1,21 +1,22 @@
 import { v4 as uuidv4 } from 'uuid';
 
+export const sortColumns = (a, b) => a.order - b.order;
+
 export const addColumn = (columns, setColumns) => {
   setColumns([
     ...columns,
     {
       id: uuidv4(),
+      columns: [],
       name: '',
       slug: '',
       field: 'TEXT',
-      value: '',
+      data: {
+        value: '',
+      },
       unsaved: true,
     },
   ]);
-};
-
-export const addSubColumn = (column, columns, setColumns) => {
-
 };
 
 export const updateColumn = (column, columns, setColumns) => {
@@ -27,6 +28,27 @@ export const updateColumn = (column, columns, setColumns) => {
   ]);
 };
 
+export const addSubColumn = (column, columns, setColumns) => {
+  const updatedColumn = {
+    ...column,
+    columns: [
+      ...column.columns,
+      {
+        id: uuidv4(),
+        columns: [],
+        name: '',
+        slug: '',
+        field: 'TEXT',
+        data: {
+          value: '',
+        },
+        unsaved: true,
+      },
+    ],
+  };
+  updateColumn(updatedColumn, columns, setColumns);
+};
+
 export const deleteColumn = (column, columns, setColumns) => {
   const columnIdx = columns.findIndex((c) => c.id === column.id);
   setColumns([
@@ -35,8 +57,38 @@ export const deleteColumn = (column, columns, setColumns) => {
   ]);
 };
 
+export const updateSubColumn = (subColumn, parentColumn, columns, setColumns) => {
+  const columnIdx = parentColumn.columns.findIndex((c) => c.id === subColumn.id);
+  const updatedColumn = {
+    ...parentColumn,
+    columns: [
+      ...parentColumn.columns.slice(0, columnIdx),
+      subColumn,
+      ...parentColumn.columns.slice(columnIdx + 1),
+    ],
+  };
+
+  updateColumn(updatedColumn, columns, setColumns);
+};
+
+export const deleteSubColumn = (subColumn, parentColumn, columns, setColumns) => {
+  const columnIdx = parentColumn.columns.findIndex((c) => c.id === subColumn.id);
+  const updatedColumn = {
+    ...parentColumn,
+    columns: [
+      ...parentColumn.columns.slice(0, columnIdx),
+      ...parentColumn.columns.slice(columnIdx + 1),
+    ],
+  };
+  updateColumn(updatedColumn, columns, setColumns);
+};
+
 export default {
   addColumn,
   updateColumn,
   deleteColumn,
+  addSubColumn,
+  updateSubColumn,
+  deleteSubColumn,
+  sortColumns,
 };

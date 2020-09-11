21eeaacd0ebf33c63283e067332cac03eea27dae
kcunanan <kevin.a.cunanan@gmail.com>
2020-09-11 01:34:45 -0700

    fixing deploy script by removing chmod

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,6 @@ ssh -o StrictHostKeyChecking=no ec2-user@$EC2_PUBLIC_IP_ADDRESS << 'ENDSSH'
   cd /home/ec2-user/app
   export $(cat .env)
   rm .env
-  chmod +x ./init-letsencrypt.sh
   echo $DOCKER_TOKEN | docker login -u $DOCKER_USERNAME --password-stdin
   docker system prune -f
   docker pull floatcms/api

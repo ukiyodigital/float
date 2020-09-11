cce4f92598cef38944932288f8014ad732b04935
kcunanan <kevin.a.cunanan@gmail.com>
2020-09-11 00:23:44 -0700

    decided to manually trigger letsencrypt

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,6 @@ ssh -o StrictHostKeyChecking=no ec2-user@$EC2_PUBLIC_IP_ADDRESS << 'ENDSSH'
   export $(cat .env)
   rm .env
   chmod +x ./init-letsencrypt.sh
-  sudo ./init-letsencrypt.sh
   echo $DOCKER_TOKEN | docker login -u $DOCKER_USERNAME --password-stdin
   docker system prune -f
   docker pull floatcms/api

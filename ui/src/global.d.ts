c7ec963a903d3c0325a6395eee14d03fb481ddb9
kcunanan <kevin.a.cunanan@gmail.com>
2020-10-22 23:14:24 -0700

    fixed typings

--- ui/src/global.d.ts
+++ ui/src/global.d.ts
@@ -1,13 +1,40 @@
+type ColumnValue = string | Record<string, unknown> | { [key: string]: unknown } | unknown;
+
+interface Column {
+    id?: string;
+    name: string;
+    slug: string;
+    field: string;
+    value?: ColumnValue;
+    columns: Column[];
+    data?: ColumnValue;
+    unsaved?: boolean;
+    order: number;
+    page_id?: number | null;
+    __typename?: string;
+}
+
 interface Page {
     id: string;
     name: string;
     slug: string;
+    columns?: Column[];
+    site: Site;
+    __typename: string;
 }
 
 interface Flock {
     id: string;
     name: string;
     slug: string;
+    columns?: Column[];
+    data?: any;
+    site: Site;
+    __typename: string;
+}
+
+interface APIKey {
+    key: string;
 }
 
 interface Site {
@@ -16,4 +43,31 @@ interface Site {
     slug: string;
     pages?: Page[];
     flocks?: Flock[];
+    owner?: {
+        username: string;
+    }
+    apiKey?: APIKey[];
 }
+
+// UI Interfaces
+interface Field {
+    name: string;
+    label: string | Node | JSX.Element;
+    type?: string;
+    value?: ColumnValue;
+    onChange?(value: any): void;
+    setValue?(name: string, value: any, config?: Record<string, unkown>): void;
+}
+
+interface FieldError {
+    key: string | number;
+    message: string;
+    errorType: string;
+}
+
+// Hook Error
+interface FloatError {
+    key: number | string;
+    message: string;
+    errorType: string;
+  }

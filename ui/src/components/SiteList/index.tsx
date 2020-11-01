44cc697f61b83c4b0941b174c496691b6f730d1c
kcunanan <kevin.a.cunanan@gmail.com>
2020-10-31 22:31:51 -0700

    created index.tsx to import breadcrumb and primary component in one import

---
+++ ui/src/components/SiteList/index.tsx
@@ -0,0 +1,7 @@
+import SiteList from '_/components/SiteList/SiteList';
+import SiteBreadcrumbAction from '_/components/SiteList/SiteBreadcrumbAction/SiteBreadcrumbAction';
+
+export {
+  SiteList,
+  SiteBreadcrumbAction,
+};

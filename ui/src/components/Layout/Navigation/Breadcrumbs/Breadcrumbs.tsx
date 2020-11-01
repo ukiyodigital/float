2a8ca59395c2146ac4f5a68c4622673078094af4
kcunanan <kevin.a.cunanan@gmail.com>
2020-10-31 22:28:20 -0700

    refactored to add component logic to breadcrumb

--- ui/src/components/Layout/Navigation/Breadcrumbs/Breadcrumbs.tsx
+++ ui/src/components/Layout/Navigation/Breadcrumbs/Breadcrumbs.tsx
@@ -1,103 +1,82 @@
 import React from 'react';
 
-import { makeStyles, Theme } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import { useGetSiteQuery } from '_/hooks';
 
-import { NavLink, LinkProps } from 'react-router-dom';
-
-import { Link, Breadcrumbs } from '@material-ui/core';
+import { Breadcrumbs } from '@material-ui/core';
 import ArrowRightIcon from '@material-ui/icons/ArrowRight';
 
+import BreadcrumbLink from '_/components/Layout/Navigation/Breadcrumbs/BreadcrumbLink/BreadcrumbLink';
 
-interface BreadcrumbLinkProps {
-  to: string;
-  exact?: boolean
-  className?: string;
-  children?: JSX.Element | string;
-}
-
-const useStyles = makeStyles((theme: Theme) => ({
-  root: {
-    marginBottom: '2rem',
-  },
-  breadcrumb: {
-    color: theme.palette.breadcrumb.main,
-    fontWeight: 300,
-    fontSize: '28px',
-    '&.active': {
-      color: theme.palette.breadcrumb.dark,
-      textDecorationLine: 'underline',
-    }
-  },
-}));
-
-const BreadcrumbLink: React.FC<BreadcrumbLinkProps> = (props) => {
-  const { to, exact = false, children } = props;
-  const classes = useStyles();
-  const renderLink = React.useMemo(
-    () =>
-      React.forwardRef<HTMLAnchorElement, Omit<LinkProps, 'to'>>((itemProps, ref) => (
-        <NavLink exact={exact} to={to} ref={ref} {...itemProps} />
-      )),
-    [to],
-  );
-
-  return (
-    <Link
-      className={classes.breadcrumb}
-      component={renderLink}
-    >
-      {children}
-    </Link>
-  )
-};
 
 interface Props {
+  component?: React.FC | JSX.Element,
   params: {
     [key: string]: string;
   }
 }
 
-const FloatBreadcrumbs: React.FC<Props> = ({ params }) => {
+const useStyles = makeStyles(() => ({
+  root: {
+    display: 'flex',
+    '& .end': {
+
+    },
+  },
+  breadcrumbs: {
+    marginBottom: '2rem',
+    flex: 1,
+  }
+}));
+
+const FloatBreadcrumbs: React.FC<Props> = ({ params, component }) => {
   const classes = useStyles();
   const { siteSlug = "", flockSlug = "", pageSlug = "" } = params;
   const [, currentSite] = useGetSiteQuery(siteSlug);
 
   const { name = '', pages = [], flocks = []}: { name: string, pages: Page[], flocks: Flock[] } = currentSite || {};
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const Component: any = component;
+
   return (
-    <Breadcrumbs className={classes.root} separator={<ArrowRightIcon fontSize="small" />} aria-label="breadcrumb">
-      <BreadcrumbLink
-        to="/site"
-        exact
-      >
-        Projects
-      </BreadcrumbLink>
-      {currentSite && (
+    <div className={classes.root}>
+      <Breadcrumbs className={classes.breadcrumbs} separator={<ArrowRightIcon fontSize="small" />} aria-label="breadcrumb">
         <BreadcrumbLink
-          to={`/site/${siteSlug}`}
+          to="/site"
           exact
         >
-            {name}
+          Projects
         </BreadcrumbLink>
-      )}
-      {(pageSlug && currentSite) && (
-        <BreadcrumbLink
-          exact
-          to={`/site/${siteSlug}/page/${pageSlug}/edit`}
-        >
-          {pages.find(page => page.slug === pageSlug)?.name || ''}
-        </BreadcrumbLink>
-      )}
-      {flockSlug && (
-        <BreadcrumbLink
-          exact
-          to={`/site/${siteSlug}/flock/${flockSlug}/edit`}
-        >
-          {flocks.find(flock => flock.slug === flockSlug)?.name || ''}
-        </BreadcrumbLink>
-      )}
-    </Breadcrumbs>
+        {currentSite && (
+          <BreadcrumbLink
+            to={`/site/${siteSlug}`}
+            exact
+          >
+              {name}
+          </BreadcrumbLink>
+        )}
+        {(pageSlug && currentSite) && (
+          <BreadcrumbLink
+            exact
+            to={`/site/${siteSlug}/page/${pageSlug}/edit`}
+          >
+            {pages.find(page => page.slug === pageSlug)?.name || ''}
+          </BreadcrumbLink>
+        )}
+        {flockSlug && (
+          <BreadcrumbLink
+            exact
+            to={`/site/${siteSlug}/flock/${flockSlug}/edit`}
+          >
+            {flocks.find(flock => flock.slug === flockSlug)?.name || ''}
+          </BreadcrumbLink>
+        )}
+      </Breadcrumbs>
+      <div className="end">
+        {component && (<Component />)}
+      </div>
+    </div>
   );
 };
 

187971d5e3595379b35f5d983c3485183073261b
kcunanan <kevin.a.cunanan@gmail.com>
2020-11-01 01:13:35 -0700

    added settings icon to detail

--- ui/src/routing/routes.ts
+++ ui/src/routing/routes.ts
@@ -7,6 +7,8 @@ import SiteDetail from '_/components/SiteDetail/SiteDetail';
 import EditPage from '_/components/EditPage/EditPage';
 import EditFlock from '_/components/EditFlock/EditFlock';
 
+import SettingsAction from '_/components/Common/SettingsAction/SettingsAction';
+
 export const defaultPath = '/site';
 
 export const routes = [
@@ -14,7 +16,7 @@ export const routes = [
     name: 'sites', path: '/site', exact: true, component: SiteList, loginRequired: true, breadcrumbs: true, breadcrumbComponent: SiteBreadcrumbAction,
   },
   {
-    name: 'site-detail', path: '/site/:siteSlug', exact: true, component: SiteDetail, loginRequired: true, breadcrumbs: true,
+    name: 'site-detail', path: '/site/:siteSlug', exact: true, component: SiteDetail, loginRequired: true, breadcrumbs: true, breadcrumbComponent: SettingsAction,
   },
   {
     name: 'edit-flock', path: '/site/:siteSlug/flock/:flockSlug/edit', exact: true, component: EditFlock, sidebar: true, loginRequired: true, breadcrumbs: true,
